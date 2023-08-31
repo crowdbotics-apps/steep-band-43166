b9e59443ff2ccdfe99a3faa4875f4fc9d9968feb
Crowdbotics <team@crowdbotics.com>
2023-08-31 13:51:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-band-43166",
+  url: "https://steep-band-43166.botics.co",
+  api: "https://steep-band-43166.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
